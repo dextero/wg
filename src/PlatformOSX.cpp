b5b57f9e59fad7222183310abe565cdc91504d22
Piotr Stefaniak <475801+pstefaniak@users.noreply.github.com>
2011-02-02 14:55:42 +0100

    synchro do r1966

--- src/PlatformOSX.cpp
+++ src/PlatformOSX.cpp
@@ -1,30 +1,34 @@
 #ifdef PLATFORM_MACOSX
+#ifndef PLATFORM_IPHONE
 extern "C" const char * GetUserDir_OSX();
 
+#include <string>
+#include "Utils/FileUtils.h"
 static std::string gUserDir = "~/Library/WarlocksGauntlet";
 const std::string & FileUtils::GetUserDir()
 {
 	static bool sInitialized = false;
 	if (!sInitialized) {
-		gUserDir = GetUserDir_OSX() + "/Library/WarlocksGauntlet;
-        fprintf(stderr, "Detected userDir as `%s'\n", gUserDir.c_str());
-        gInitialized = true;
+		gUserDir = std::string(GetUserDir_OSX()) + "/Library/WarlocksGauntlet";
+		fprintf(stderr, "Detected userDir as `%s'\n", gUserDir.c_str());
+		sInitialized = true;
 	}
-    return gUserDir;
+	return gUserDir;
 }
 
 extern "C" void CreateDir_OSX(const char * path);
 
 void FileUtils::CreateDir(const char * filename)
 {
-    fprintf(stderr, "Creating directory `%s'...\n", filename);
-    CreateDirectory_OSX(filename);
+	fprintf(stderr, "Creating directory `%s'...\n", filename);
+	CreateDir_OSX(filename);
 }
 
 extern "C" int AskForFullscreen_OSX(const wchar_t * title, const wchar_t * message, int maxw, int maxh);
 
 bool AskForFullscreen(const wchar_t * title, const wchar_t * message, int maxw, int maxh)
 {
-	return (bool)AskForFullscreen_OSX(titleStr.c_str(), messageStr.c_str(), maxw, maxh);
+	return (bool)AskForFullscreen_OSX(title, message, maxw, maxh);
 }
+#endif // iphone
 #endif //PLATFORM_MACOSX
