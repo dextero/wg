41acd1bbaefe5d491afbd2b97ca2592ca31ff35b
cixot <cixot@localhost>
2011-02-28 18:09:10 +0100

    upgrade do sfml 1.6

--- tools/linux/scripts/deploy.bash
+++ tools/linux/scripts/deploy.bash
@@ -27,14 +27,14 @@ hg archive -I user deploy
 cp data.vfs deploy
 cp tools/linux/launcher/WarlocksGauntlet deploy
 mkdir deploy/libs32
-cp libs32/libsfml-graphics.so.1.4 deploy/libs32
-cp libs32/libsfml-window.so.1.4 deploy/libs32
-cp libs32/libsfml-system.so.1.4 deploy/libs32
-cp libs32/libsfml-audio.so.1.4 deploy/libs32
+cp libs32/libsfml-graphics.so.1.6 deploy/libs32
+cp libs32/libsfml-window.so.1.6 deploy/libs32
+cp libs32/libsfml-system.so.1.6 deploy/libs32
+cp libs32/libsfml-audio.so.1.6 deploy/libs32
 cp libs32/libopenal.so.1 deploy/libs32
 cp libs32/libsndfile.so.1 deploy/libs32
 mkdir deploy/bin
 cp $binary deploy/bin
 
-g++ tools/linux/check_fullscreen/main.cpp -o deploy/bin/check_fullscreen -static-libgcc -O2 /usr/lib/gcc/i486-linux-gnu/4.4.1/libstdc++.a -I"build/includes/SFML-1.4/include" -L"./libs32" -lsfml-graphics -lsfml-window -lsfml-system || fail
+g++ tools/linux/check_fullscreen/main.cpp -o deploy/bin/check_fullscreen -static-libgcc -O2 /usr/lib/gcc/i486-linux-gnu/4.4.1/libstdc++.a -I"build/includes/SFML-1.6/include" -L"./libs32" -lsfml-graphics -lsfml-window -lsfml-system || fail
 
