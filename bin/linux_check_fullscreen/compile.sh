3ee205601be6994cf99aae0ac8989792cde5fa5d
Piotr Stefaniak <475801+pstefaniak@users.noreply.github.com>
2011-06-29 01:09:28 +0200

    removing duplicate, linux_check_fullscreen is in /tools/linux/...

--- bin/linux_check_fullscreen/compile.sh
+++
@@ -1,2 +0,0 @@
-#/bin/bash
-g++ main.cpp -o ../check_fullscreen -static-libgcc -O2 /usr/lib/gcc/i486-linux-gnu/4.4.1/libstdc++.a -I"../../build/includes/SFML-1.6/include" -L"../../lib" -lsfml-graphics -lsfml-window -lsfml-system
