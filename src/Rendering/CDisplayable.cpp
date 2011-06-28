5f0bb58a7850f1f07f3c58ae2fd42a32f194a0fe
Marcin Radomski <dextero@users.noreply.github.com>
2011-06-28 16:12:39 +0200

    re #1047 - GL_CLAMP_TO_EDGE, czyli ostatecznie zamkni?cie kwestii przerw mi?dzy kaflami

--- src/Rendering/CDisplayable.cpp
+++ src/Rendering/CDisplayable.cpp
@@ -113,7 +113,6 @@ void CDisplayable::SetStaticImage(CImage *img, int number, float extraPixels)
 
     mSFSprite->FlipX( false );
 	if (img != NULL) {
-        img->SetSmooth(false);
         mSFSprite->SetImage( *img );
 		sf::IntRect rect = img->GetRect(number);
 		rect.Left += (int) extraPixels;		rect.Top += (int) extraPixels;
