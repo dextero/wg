71c95df793a2269e1eafa603c61e9aaae3d5ca60
dextero <dextero@localhost>
2011-07-14 21:27:16 +0200

    poprawka, zablakane CKeyReader::

--- src/GUI/CKeyReader.h
+++ src/GUI/CKeyReader.h
@@ -38,7 +38,7 @@ namespace GUI
 		virtual bool OnMouseEvent( float x, float y, mouseEvent e );
 		virtual void OnKeyEvent( const sf::Event::KeyEvent& e, bool pressed );
         
-        void CKeyReader::SetKeyFromMouseButton(int btn);
+        void SetKeyFromMouseButton(int btn);
 
 		bool mMouseOver;
         bool mActive;
