2931a49395ff921021d7db4fd24a5e9047a2ad59
cixot <cixot@localhost>
2011-07-27 23:50:03 +0200

    re #1112 - zmieniam nazwe projektu i exeka na WarlocksGauntlet

--- tools/windows/cygwin-autobuild/config.sh
+++ tools/windows/cygwin-autobuild/config.sh
@@ -2,7 +2,7 @@ export DEVENV_PATH="C:/Program Files/Microsoft Visual Studio 8/Common7/IDE/deven
 
 export WORK_DIR="./workDir"
 export SLN_PATH="$WORK_DIR/build/vs2005"
-export SLN_NAME="WarsztatGame.sln"
+export SLN_NAME="WarlocksGauntlet.sln"
 export SLN_CONFIG="Release"
 export EDITOR_PATH="E:/Documents/C++ Projects/WG-tools/WGEdytor"
 export NSIS_PATH="C:/Program Files/NSIS/makensis.exe"
