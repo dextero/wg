2303bf821081af25e17084e7ba3d6a313f15e663
Piotr Stefaniak <475801+pstefaniak@users.noreply.github.com>
2011-07-27 23:50:03 +0200

    re #1112 - zmieniam nazwe projektu i exeka na WarlocksGauntlet

--- tools/windows/cygwin-autobuild/bat.bat
+++ tools/windows/cygwin-autobuild/bat.bat
@@ -1,1 +1,1 @@
-"C:/Program Files/Microsoft Visual Studio 8/Common7/IDE/devenv.exe" ./workDir/build/vs2005/WarsztatGame.sln /build Release /out build.log
+"C:/Program Files/Microsoft Visual Studio 8/Common7/IDE/devenv.exe" ./workDir/build/vs2005/WarlocksGauntlet.sln /build Release /out build.log
--- tools/windows/cygwin-autobuild/build.sh
+++ tools/windows/cygwin-autobuild/build.sh
@@ -8,16 +8,16 @@ hg archive -I ../../../src -I ../../../build "$WORK_DIR"
 
 echo "Creating output directory..."
 CURRENT_DIR=$(pwd)
-#REVISION=$(svn info "$WORK_DIR" | grep Revision: | cut -d" " -f2 | cut -c1-4)
-#REVISION=$(hg id -i)
 REVISION=$(hg describe -l 10000)
-MY_OUTPUT_FOLDER_NAME="$OUTPUT_FOLDER_NAME-"$(date "+%d-%m-%Y")"-$REVISION"
-OUTPUT_DIR="$CURRENT_DIR/$MY_OUTPUT_FOLDER_NAME"
+DATE=`date +%F_%H-%M-%S`
+PACKAGE_NAME="wg_win32_"$REVISION"_"$DATE
+
+OUTPUT_DIR="$CURRENT_DIR/$PACKAGE_NAME"
 echo "Output dir is $OUTPUT_DIR"
 rm -rf "$OUTPUT_DIR" ; mkdir "$OUTPUT_DIR"
 
 echo "Building..."
-WG_BINARY_NAME="WarsztatGame.exe"
+WG_BINARY_NAME="WarlocksGauntlet.exe"
 mkdir "$SLN_PATH/$SLN_CONFIG"
 rm -f "$SLN_PATH/$SLN_CONFIG/$WG_BINARY_NAME"
 echo "\"$DEVENV_PATH\"" "$SLN_PATH/$SLN_NAME" /build "$SLN_CONFIG" /out "build.log" > bat.bat
@@ -30,7 +30,7 @@ if [ "$?" -ne 0 ]; then echo "build failed, $WG_BINARY_NAME not found"; exit 1; 
 cp "$CURRENT_DIR/../../../libsndfile-1.dll" "$OUTPUT_DIR/"
 cp "$CURRENT_DIR/msvcr80.dll" "$OUTPUT_DIR/"
 cp "$CURRENT_DIR/msvcp80.dll" "$OUTPUT_DIR/"
-cp "$CURRENT_DIR/WG-VC80.exe.manifest" "$OUTPUT_DIR/WarsztatGame.exe.manifest"
+cp "$CURRENT_DIR/WG-VC80.exe.manifest" "$OUTPUT_DIR/WarlocksGauntlet.exe.manifest"
 cp "$CURRENT_DIR/Microsoft.VC80.CRT.manifest" "$OUTPUT_DIR/"
 cp "$CURRENT_DIR/openal32.dll" "$OUTPUT_DIR/"
 cd "$CURRENT_DIR"
@@ -40,8 +40,8 @@ hg archive -I ../../../build/wg64.ico "$OUTPUT_DIR/wg64.ico"
 
 echo "Zipping package..."
 cd "$CURRENT_DIR"
-PACKAGE_ZIP="$MY_OUTPUT_FOLDER_NAME.zip"
-zip -q -r "$PACKAGE_ZIP" "$MY_OUTPUT_FOLDER_NAME"
+PACKAGE_ZIP="$PACKAGE_NAME.zip"
+zip -q -r "$PACKAGE_ZIP" "$PACKAGE_NAME"
 
 echo "Uploading..."
 scp "$PACKAGE_ZIP" toxic@hell.org.pl:public_html/all/warsztat/snapshots/win32/en
