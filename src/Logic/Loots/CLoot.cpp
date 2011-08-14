333be8dbfc1cd55688303409327f5335a0590818
cixot <cixot@localhost>
2011-08-14 20:30:40 +0200

    wip, chcialbym dodac itemom kontekst wykonania, zeby dalo sie tworzyc itemy o ustalonym 'level' albo 'power'

--- src/Logic/Loots/CLoot.cpp
+++ src/Logic/Loots/CLoot.cpp
@@ -6,7 +6,12 @@
 //#include "../OptionChooser/CSimpleOptionHandler.h"
 #include "../../Input/CPlayerController.h"
 
-CLoot::CLoot(const std::wstring& uniqueId) : CPhysical(uniqueId), obj(NULL), mOptionHandler(NULL), mItem(NULL)
+CLoot::CLoot(const std::wstring& uniqueId) : 
+        CPhysical(uniqueId),
+        obj(NULL),
+        mOptionHandler(NULL),
+        mItem(NULL),
+        mLevel(0)
 {
     SetZIndex(Z_LOOT);
     SetCategory(PHYSICAL_LOOT);
@@ -48,7 +53,14 @@ void CLoot::HandleCollision(CPlayer * player)
     }
 
     if (obj->effect) {
-        obj->effect->Apply(player);
+        if (mLevel != 0) {
+            fprintf(stderr, "loot effect of level %d\n", mLevel);
+            EffectSourcePtr source = EffectSourcePtr(NULL);
+            ExecutionContextPtr context = ExecutionContextPtr(NULL);
+            obj->effect->Apply(player, source, context);
+        } else {
+            obj->effect->Apply(player);
+        }
     }
     MarkForDelete();
 }
