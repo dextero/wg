95b0173e11a9e1a17d13a9b2d26f0aa10cf1de00
cixot <cixot@localhost>
2012-01-01 20:28:43 +0100

    re #1205 troche npcow sprzedajacych jakies losowe czary

--- tools/windows/cygwin-autobuild/bat.bat
+++ tools/windows/cygwin-autobuild/bat.bat
@@ -1,2 +1,2 @@
-SET CL=/DSCM_REVISION#1.1-66-37120c7894ba
+SET CL=/DSCM_REVISION#1.1-88-e86a1c5a0e60
 "C:/Program Files/Microsoft Visual Studio 8/Common7/IDE/devenv.exe" ./workDir/build/vs2005/WarlocksGauntlet.sln /build Release /out build.log
