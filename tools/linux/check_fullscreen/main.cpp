4cdde35491ce3315515b6dda0933ae1d141cf479
Piotr Stefaniak <475801+pstefaniak@users.noreply.github.com>
2012-02-14 18:15:36 +0100

    costam poprawiam

--- tools/linux/check_fullscreen/main.cpp
+++ tools/linux/check_fullscreen/main.cpp
@@ -7,10 +7,10 @@ int main(int argc, char* argv[]){
     const unsigned int windowMode = sf::Style::Close;
     const unsigned int fullscreenMode = sf::Style::Fullscreen;
 
-    int maxw = 800; maxh = 600;
+    int maxw = 800, maxh = 600;
     for (size_t i = 0; i < sf::VideoMode::GetModesCount(); i++)
     {
-        videoMode = sf::VideoMode::GetMode(i);
+        sf::VideoMode videoMode = sf::VideoMode::GetMode(i);
         if (videoMode.Width > maxw) {
             maxw = videoMode.Width; maxh = videoMode.Height;
         }
