c6544a5adfea713faa30dcf71e0fab4950602afa
Piotr Stefaniak <475801+pstefaniak@users.noreply.github.com>
2012-04-18 16:12:26 +0200

    new branch

--- tools/windows/cygwin-autobuild/bat.bat
+++ tools/windows/cygwin-autobuild/bat.bat
@@ -1,2 +1,2 @@
-SET CL=/DSCM_REVISION#1.1-131-4268aca888e8
+SET CL=/DSCM_REVISION#1.3
 "C:/Program Files/Microsoft Visual Studio 8/Common7/IDE/devenv.exe" ./workDir/build/vs2005/WarlocksGauntlet.sln /build Release /out build.log
